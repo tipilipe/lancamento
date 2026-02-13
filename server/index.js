60dff2e328c4ba3b364dd66124ef295e008949b2
Seu Nome <filipem.s@outlook.com>
2026-02-13 18:19:03 -0300

    Fix: Sintaxe de rota curinga para Express 5

--- server/index.js
+++ server/index.js
@@ -330,6 +330,6 @@ app.listen(PORT, () => {
 });
 
 // Fallback to index.html for SPA routing
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
